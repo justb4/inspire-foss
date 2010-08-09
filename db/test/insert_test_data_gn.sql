a11354e0c251fd0151f3d1d00072379a4b28730f
oudwijfman <oudwijfman@gmail.com>
2010-08-09 10:23:46 +0000

    Changes in database schema and WFS configuration for GN:localType

--- db/test/insert_test_data_gn.sql
+++ db/test/insert_test_data_gn.sql
@@ -24,6 +24,15 @@ insert into md_resolution(
 	,1000000
 );
 
+insert into md_resolution(
+	id
+	,distance
+) values (
+	3
+	,10,7
+);
+
+-- Amsterdam
 insert into inspireid(
 	localid
 	,namespace
@@ -31,7 +40,6 @@ insert into inspireid(
 	'1'
 	,'NL.Geodan.Test'
 );
-
 
 insert into namedplace(
 	inspireid
@@ -41,8 +49,8 @@ insert into namedplace(
 ) values (
 	(select id from inspireid where localid = '1')
 	,st_pointfromtext('POINT(52.36 4.92)', 4258) -- note the axis order: latitude, longitude
-	,2
 	,1
+	,2
 );
 
 insert into placetypes(
@@ -109,4 +117,111 @@ insert into spellingofname(
 	,'Latn'
 );
 
+-- Rotterdam
+insert into inspireid(
+	localid
+	,namespace
+) values (
+	'2'
+	,'NL.Geodan.Test'
+);
 
+insert into namedplace(
+	inspireid
+	,geometry
+	,leastdetailedviewingresolution
+	,mostdetailedviewingresolution
+) values (
+	(select id from inspireid where localid = '2')
+	,st_pointfromtext('POINT(51.92 4.5)', 4258) -- note the axis order: latitude, longitude
+	,1
+	,3
+);
+
+insert into placetypes(
+	namedplace 
+	,namedplacetype 
+) values (
+	(select id from namedplace where inspireid = (select id from inspireid where localid = '2'))
+	,'administrativeUnit'
+);
+
+insert into placetypes(
+	namedplace 
+	,namedplacetype 
+) values (
+	(select id from namedplace where inspireid = (select id from inspireid where localid = '2'))
+	,'populatedPlace'
+);
+
+insert into geographicalname(
+	namedplace
+	,language
+	,nativeness
+	,namestatus
+	,sourceofname
+	,grammaticalgender
+	,grammaticalnumber
+) values (
+	(select currval('namedplace_id'))
+	,'dut'
+	,'endonym'
+	,'official'
+	,'Geodan'
+	,'neuter'
+	,'singular'
+);
+
+insert into spellingofname(
+	geographicalname
+	,text
+	,script
+) values (
+	(select currval('geographicalname_id'))
+	,'Rotterdam'
+	,'Latn'
+);
+
+insert into localisedcharacterstring (
+	language
+	,country
+	,characterencoding
+	,text
+) values 
+(
+'dut'
+,'NL'
+,'utf8'
+,'Provinciehoofdstad'
+);
+
+insert into localtype (
+ namedplace
+ ,localisedcharacterstring
+) values
+(
+	(select id from namedplace where inspireid = (select id from inspireid where localid = '2'))
+	,	(select currval('localisedcharacterstring_id'))
+);
+
+insert into localisedcharacterstring (
+	language
+	,country
+	,characterencoding
+	,text
+) values 
+(
+'dut'
+,'NL'
+,'utf8'
+,'Stad'
+);
+
+insert into localtype (
+ namedplace
+ ,localisedcharacterstring
+) values
+(
+	(select id from namedplace where inspireid = (select id from inspireid where localid = '2'))
+	,	(select currval('localisedcharacterstring_id'))
+);

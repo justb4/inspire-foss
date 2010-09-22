dc25ce542da7e23f22f9b8862de31e6d11e244c3
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2010-09-22 16:09:59 +0000

    first version transformation Dutch local Admin Units in MapInfo to INSPIRE AU GML

---
+++ etl/NL.Kadaster/AdministrativeUnits/mapinfo-to-gml.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Transforms Dutch local Admin Units in MapInfo to flat GML
+#
+# Author: Just van den Broecke
+#
+
+# Transformation
+function transform() {
+    echo "transforming $1 to $2"
+
+    # Convert MapInfo to flat GML, also transforms coordinates to INSPIRE ETRS89
+    ogr2ogr -overwrite  -t_srs "EPSG:4258" -s_srs "EPSG:28992"  -f "GML" $2 $1
+
+    # Dutch source is encoded in Windows LATIN1 :-( (CP1252)
+    # need to make UTF-8 encoded GML
+    cat $2 | iconv -f CP1252 -t UTF-8 > temp.gml
+    mv temp.gml $2
+
+    echo "transforming $1 to $2 DONE"
+}
+
+
+# transform Ned_Burgem.TAB gemeente.gml
+# transform Ned_Provincie.TAB provincie.gml
+# transform KDXD720_2010.TAB koppel.gml
+# transform Nederland.TAB  nederland.gml
---
+++ etl/NL.Kadaster/AdministrativeUnits/test/test-gemeente.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Transforms Dutch local Admin Units to INSPIRE AU
+#
+# Author: Just van den Broecke
+#
+
+cd ..
+./local-to-inspire-au.sh test/gemeente.xml  test/gemeente-au.xml
+
+
+
+

3441602f6f0d76a9e4755805f5f8ff366a639094
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2010-09-28 12:51:49 +0000

    added ETL for provinces and country level for NL.Kadaster

--- etl/NL.Kadaster/bin/to-gml.sh
+++ etl/NL.Kadaster/bin/to-gml.sh
@@ -8,6 +8,7 @@
 
 # Transformation
 function transform() {
+    # really need this specific PROJ projection strings as most transforms from 28992 are broken
     proj28992="+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs"
     proj4258="+proj=longlat +ellps=GRS80 +datum=WGS84 +no_defs  <>"
     echo "transforming $1 to $2"

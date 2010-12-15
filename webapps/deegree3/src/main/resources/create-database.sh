5e02a9ab55e4cd2f5d115a6db385417cd7d22af4
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2010-12-15 13:15:12 +0000

    add support for ESDIN ExM in deegree3 WFS webapp (also fixes ExM XSD issues)

--- webapps/deegree3/src/main/resources/create-database.sh
+++ webapps/deegree3/src/main/resources/create-database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run this file on the database server host or edit the pgpass.conf file.
 export PGPASSFILE=pgpass.conf
-psql -a -U kadmin -d postgres -f create-database.sql -v db_name=inspire_blob
+psql -a -U kadmin -d postgres -f create-database.sql -v db_name=inspire-blob

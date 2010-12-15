89fa3d88e3cf4662753f2de0f84b0f273d3f7f41
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2010-12-15 17:28:15 +0000

    ETL for Admin Units from INSPIRE AU to ESDIN ExM AU

--- etl/NL.Kadaster/ExM/test/transform.sh
+++ etl/NL.Kadaster/ExM/test/transform.sh
@@ -1,5 +1,6 @@
 cd ..
 ./inspire-cp-to-exm-cp.sh test/kad-cp.gml  test/kad-cp-exm.gml
 ./inspire-gn-to-exm-gn.sh test/kad-gn.gml  test/kad-gn-exm.gml
+./inspire-au-to-exm-au.sh test/kad-au.gml  test/kad-au-exm.gml
 
 cd -

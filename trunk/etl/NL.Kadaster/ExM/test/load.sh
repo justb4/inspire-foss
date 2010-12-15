906c3c98c602524f7116e2cb6a8fbcd15785d08f
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2010-12-15 15:11:33 +0000

    ETL for GeographicalNames from INSPIRE GN to ESDIN ExM GN

--- trunk/etl/NL.Kadaster/ExM/test/load.sh
+++ trunk/etl/NL.Kadaster/ExM/test/load.sh
@@ -1,3 +1,4 @@
 load="../../../../tools/loader/bin/fsloader.sh  inspire-postgis inspire_blob GML_32 USE_EXISTING"
 
 $load kad-cp-exm.gml
+$load kad-gn-exm.gml
--- trunk/etl/NL.Kadaster/ExM/test/transform.sh
+++ trunk/etl/NL.Kadaster/ExM/test/transform.sh
@@ -1,4 +1,5 @@
 cd ..
 ./inspire-cp-to-exm-cp.sh test/kad-cp.gml  test/kad-cp-exm.gml
+./inspire-gn-to-exm-gn.sh test/kad-gn.gml  test/kad-gn-exm.gml
 
 cd -

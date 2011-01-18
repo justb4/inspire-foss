2912368e234ce006d7440f70d60c9f6b871aca89
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2011-01-18 16:14:17 +0000

    add railway to TN ETL (was removed somehow)

--- etl/NL.Kadaster/bin/tn-extract.sh
+++ etl/NL.Kadaster/bin/tn-extract.sh
@@ -16,7 +16,7 @@ SETTINGS_SCRIPT="settings-`hostname`.sh"
 # cd $GEODATA_HOME/top10nl/shape/testgebied
 DATA_ROOT_DIR=$GEODATA_HOME/top10nl/shape/oost-nl
 DATA_RESULT_DIR=$DATA_ROOT_DIR/transformed
-FEATURE_TYPES="WEGDEEL_PUNT WEGDEEL_LIJN WEGDEEL_VLAK"
+FEATURE_TYPES="WEGDEEL_PUNT WEGDEEL_LIJN WEGDEEL_VLAK SPOORBAANDEEL_LIJN"
 
 # Alle kaartbladen
 # 28Oost, 29West en 34Oost (het oorspronkelijke "Losser" gebied)

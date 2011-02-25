a136b1a29bbdab5cdd22020f66455d32b2728250
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2011-02-25 12:49:28 +0000

    adapt AU extract for simplified Shapefiles (after using http://mapshaper.org)

--- trunk/etl/NL.Kadaster/bin/au-extract.sh
+++ trunk/etl/NL.Kadaster/bin/au-extract.sh
@@ -13,15 +13,16 @@ cd $GEODATA_HOME/cidb/losser/adminunit
 rm -rf transformed
 mkdir transformed
 
-
+# Generate the ESRI Shapefiles from the MapInfo TAB files
 $TO_SHAPE_SCRIPT Ned_Burgem.TAB transformed/au_gemeenten.shp
-$TO_GML_SCRIPT transformed/au_gemeenten.shp transformed/gemeenten.gml
-
 $TO_SHAPE_SCRIPT Ned_Provincie.TAB transformed/au_provincies.shp
-$TO_GML_SCRIPT transformed/au_provincies.shp transformed/provincies.gml
-
 $TO_SHAPE_SCRIPT Nederland.TAB transformed/au_land.shp
-$TO_GML_SCRIPT transformed/au_land.shp transformed/land.gml
+
+# NB we use the simplified version of the above shapefiles
+# these were reduced to 20% using http://mapshaper.org
+$TO_GML_SCRIPT simplified20/au_gemeenten.shp transformed/gemeenten.gml
+$TO_GML_SCRIPT simplified20/au_provincies.shp transformed/provincies.gml
+$TO_GML_SCRIPT simplified20/au_land.shp transformed/land.gml
 
 ls -l transformed
 cd -

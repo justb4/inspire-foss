322d5eb59b4c27187d58b727a605b3eabb5076e7
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2011-03-21 15:43:25 +0000

    use standard user postgres to create inspire DB

--- webapps/deegree3/src/main/resources/create-database.sh
+++ webapps/deegree3/src/main/resources/create-database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Run this file on the database server host or edit the pgpass.conf file.
 export PGPASSFILE=pgpass.conf
-psql -a -U kadmin -d postgres -f create-database.sql -v db_name=inspire-blob
+psql -a -U postgres -d postgres -f create-database.sql -v db_name=inspire-blob

1c5d16c60f49e37ab82f56abed7a239e2f6737a0
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2011-08-20 12:24:30 +0000

    remove obsolete project deegree3 (based on deegree 3.0)

--- webapps/deegree3/src/main/resources/create-database.sql
+++
@@ -1,6 +0,0 @@
-drop database if exists :db_name;
-create database :db_name with owner inspire template postgis;
-\connect :db_name
-alter schema public owner to inspire;
-alter table geometry_columns owner to inspire;
-alter table spatial_ref_sys owner to inspire;
--- webapps/deegree3/src/main/resources/create-tables.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Run this file on the database server host or edit the pgpass.conf file.
-export PGPASSFILE=pgpass.conf
-psql -a -U kadmin -d postgres -f create-tables.sql -v db_name=inspire_blob

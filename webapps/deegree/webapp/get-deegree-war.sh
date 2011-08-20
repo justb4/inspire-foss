412f53f20dabb4f6b5d7b61dec72064d1a0f99de
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2011-08-20 12:56:28 +0000

    readme for deegree3.1+ setup

--- webapps/deegree/webapp/get-deegree-war.sh
+++ webapps/deegree/webapp/get-deegree-war.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ----------------------------------------------------------------------------
-# Simple script to fetch deegree .war ready-to-use distro from Maven repository
+# Simple script to fetch and build deegree .war from ready-to-use distro from Maven repository
 # TODO
 # A cleaner way would be to use a pom.xml and rely on standard Maven deps
 #
@@ -9,12 +9,26 @@
 # for 3.1 and up
 # e.g. http://artefacts.deegree.org/libs-releases-local/org/deegree/deegree-webservices/3.1-pre11/deegree-webservices-3.1-pre11.war
 
-WAR_BASE_URL=http://artefacts.deegree.org/libs-releases-local/org/deegree/deegree-webservices/
-WAR_VERSION=3.1-pre11
+# change version whenever a new one is out
+DISTRO_VERSION=3.1-pre11
+
+DISTRO_BASE_URL=http://artefacts.deegree.org/libs-releases-local/org/deegree/deegree-webservices/
+DISTRO_ZIP=deegree3.zip
+DISTRO_NAME=deegree-webservices
+DISTRO_DIR=${DISTRO_NAME}-${DISTRO_VERSION}
 WAR_FILE=deegree3.war
 
-/bin/rm ${WAR_FILE}
-wget -O ${WAR_FILE} ${WAR_BASE_URL}/${WAR_VERSION}/deegree-webservices-${WAR_VERSION}.zip
+/bin/rm -f ${DISTRO_ZIP}
+/bin/rm -rf ${DISTRO_DIR}
+/bin/rm -rf ${WAR_FILE}
+
+wget -O ${WAR_FILE} ${DISTRO_BASE_URL}/${DISTRO_VERSION}/${DISTRO_NAME}-${DISTRO_VERSION}.war
+# unzip ${DISTRO_ZIP}
+# cd ${DISTRO_DIR}/webapps/root
+# jar -cvf ../../../${WAR_FILE} *
+# cd -
+# ls
+
 
 
 

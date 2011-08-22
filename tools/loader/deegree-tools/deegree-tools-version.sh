9bf6e7fb83ac0678858f86569b3f173cdde1602d
stranger <stranger@hwbllmnn.de>
2011-08-22 15:25:09 +0000

    switched to pre12 final

--- tools/loader/deegree-tools/deegree-tools-version.sh
+++ tools/loader/deegree-tools/deegree-tools-version.sh
@@ -2,6 +2,6 @@
 #
 # Set the version here so all tools will use the same
 #
-export DEEGREE_TOOLS_VERSION=3.1-pre12-SNAPSHOT
+export DEEGREE_TOOLS_VERSION=3.1-pre12
 
 echo "Using DEEGREE_TOOLS_VERSION=${DEEGREE_TOOLS_VERSION}"

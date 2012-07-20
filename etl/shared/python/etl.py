845656a060236a95be625fd5f1172a1476a722ba
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2012-07-20 11:31:28 +0000

    more generic ETL main

--- etl/shared/python/etl.py
+++ etl/shared/python/etl.py
@@ -64,39 +64,19 @@ class ETL:
         input = factory.create_obj('input', self.configdict)
         output = factory.create_obj('output', self.configdict)
         self.transformer = factory.create_obj('transformer', self.configdict)
+        # Get (GML) docs from the input as long as available
+        while 1:
+            # 1. Extract
+            doc = input.read()
+            if doc is None:
+                log.info("DONE")
+                break
 
-        # Reusable XML parser
-        self.xml_parser = etree.XMLParser(remove_blank_text=True)
+            # 2. Transform
+            result_doc = self.transformer.transform(doc)
 
-        # Process each of the layers from the input object
-        layer_names = input.get_layer_names()
-        for layer_name in layer_names:
-            log.info("processing layer: %s" % layer_name)
-
-            # Start layer
-            input.exec_layer(layer_name)
-
-            # New splitter for each layer
-            gml_splitter = GmlSplitter(self.configdict)
-
-            # Get (GML) lines from the input as long as available
-            while 1:
-                 line = input.readline()
-                 if not line:
-                     line = input.readline_err()
-                     if not line:
-                        log.info("EOF All")
-                        break
-                 else:
-                     # Valid line: push to the splitter
-                     # If buffer filled process it
-                     buffer = gml_splitter.push_line(line)
-                     if buffer is not None:
-                        # Process/transform data in buffer
-                        result_doc = self.process_buf(buffer)
-
-                        # Write to output e.g. DB
-                        output.write(result_doc)
+            # 3. Load
+            output.write(result_doc)
 
 def main():
     # Do the ETL

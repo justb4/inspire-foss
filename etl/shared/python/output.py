bc0a400da4322b8109b514c2647e8991e9463a90
justb4 <justb4@e40802e1-0586-b17a-c8cd-49987b9bff09>
2012-07-25 13:01:13 +0000

    new outputs for ETL framework

--- etl/shared/python/output.py
+++ etl/shared/python/output.py
@@ -7,19 +7,49 @@
 #
 from postgis import PostGIS
 from util import ConfigSection, Util, etree, StringIO
+import os,sys
+
 log = Util.get_log('output')
 
-class DeegreeBlobstoreOutput:
+# Base class: pretty print XML to standard output
+class Output:
     def __init__(self, configdict):
         self.cfg = ConfigSection(configdict.items('output'))
         log.info("cfg = %s" % self.cfg.to_string())
+
+    def to_string(self, gml_doc):
+        return etree.tostring(gml_doc, pretty_print=True, xml_declaration=True, encoding='utf-8')
+
+    def write(self, gml_doc):
+        print(self.to_string(gml_doc))
+
+# Pretty print XML to file
+class FileOutput(Output):
+    def __init__(self, configdict):
+        Output.__init__(self, configdict)
+        log.info("working dir %s" %os.getcwd())
+
+    def write(self, gml_doc):
+        file_path = self.cfg.get('file_path')
+        log.info('writing to file %s' % file_path)
+        out_file = open(file_path, 'w')
+        out_file.writelines(self.to_string(gml_doc))
+        out_file.close()
+        log.info("written to %s" % file_path)
+
+# Insert features into deegree Blobstore
+class DeegreeBlobstoreOutput(Output):
+
+    def __init__(self, configdict):
+        Output.__init__(self, configdict)
         self.overwrite = self.cfg.get_bool('overwrite')
         self.feature_type_ids = {}
         self.init()
 
     def init(self):
         self.get_feature_types()
-        self.pg_srs_constraint()
+        # not required for now
+        # self.pg_srs_constraint()
         if self.overwrite:
             self.delete_features()
 
@@ -34,9 +64,6 @@ class DeegreeBlobstoreOutput:
         result = p.communicate()[0]
 
         print(result)
-
-    def publish_gml_stdout(self, s):
-        print(s)
 
     def get_feature_types(self):
         log.info('reading all featuretypes from DB')
@@ -90,7 +117,7 @@ class DeegreeBlobstoreOutput:
                 sql = "INSERT INTO gml_objects(gml_id, ft_type, binary_object) VALUES (%s, %s, %s)"
                 parameters = (gml_id, feature_type_id, db.make_bytea(blob))
             else:
-                sql = "INSERT INTO gml_objects(gml_id, ft_type, binary_object, gml_bounded_by) VALUES (%s, %s, %s, ST_GeomFromGML(%s))"
+                sql = "INSERT INTO gml_objects(gml_id, ft_type, binary_object, gml_bounded_by) VALUES (%s, %s, %s, ST_GeomFromGML(%s) )"
                 parameters = (gml_id, feature_type_id, db.make_bytea(blob), geom_str)
 
             db.uitvoeren(sql, parameters)
@@ -98,3 +125,23 @@ class DeegreeBlobstoreOutput:
 
         db.connection.commit()
         log.info("inserted %s features" % count)
+
+# Insert features via deegree FSLoader
+class DeegreeFSLoaderOutput(Output):
+
+    def __init__(self, configdict):
+        Output.__init__(self, configdict)
+
+    def write(self, gml_doc):
+        from subprocess import Popen, PIPE, STDOUT
+        fs_loader = self.cfg.get('file_path')
+
+        p = Popen([fs_load, 'inspire-postgis', 'inspire_blob', 'GML_32', 'USE_EXISTING',
+                '/dev/stdin'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+        p.stdin.write(self.to_string(gml_doc))
+
+        result = p.communicate()[0]
+
+         # print(result)
+
